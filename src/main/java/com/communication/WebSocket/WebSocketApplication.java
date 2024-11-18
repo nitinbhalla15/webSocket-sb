64cb20857d6343c46f1ae828631b351b1795ad62
Nitin <nitinbhalla15@gmail.com>
2024-11-19 00:51:57 +0530

    Initial commit

---
+++ src/main/java/com/communication/WebSocket/WebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.communication.WebSocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketApplication.class, args);
+	}
+
+}
